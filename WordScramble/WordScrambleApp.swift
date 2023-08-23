848bcc1c20ce144bb8118cf36373f99cb6077c93
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-23 23:23:13 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Lahiru Amarasooriya on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
